b80ee3b90390d0b137df852b092b6b470bd7b86c
ChristinaCode <christina._.nguyen@outlook.com>
2022-12-09 23:36:30 -0800

    code

--- app_server.R
+++ app_server.R
@@ -13,21 +13,3 @@ co2_dataset <- read.csv("https://raw.githubusercontent.com/owid/co2-data/master/
 #Which country has the highest population which country has the lowest population? 
 #Which year has the most CO2 emissions in which year has the least CO2 emissions?
 
-server <- function(input, output) {
-  compare1 <- reactive ({
-    compare1 <- df1 %>%
-      filter(df.country == input$country_one) %>%
-      mutate(country = df.country, co2 = df.co2_per_capita) %>%
-      filter((df.year) > input$year_range[1], (df.year) < input$year_range[2] %>%
-               select((df.year), country, co2)
-  })
-    compare2 <- reactive ({
-      compare2 <- df1 %>%
-        filter(df.country == input$country_two) %>%
-        mutate(country = df.country, co2 = df.co2_per_capita) %>%
-        filter((df.year) > input$year_range[1], (df.year) < input$year_range[2]) %>%
-        select((df.year), country, co2)
-      
-      country_one_name = as.character(input$country_one)
-      country_two_name = as.character(input$country_two)
-    })
