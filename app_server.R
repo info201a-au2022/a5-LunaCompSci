927cb54e1ec3d48d652d005aee82b0eed4819926
ChristinaCode <christina._.nguyen@outlook.com>
2022-12-09 23:33:29 -0800

    code

--- app_server.R
+++ app_server.R
@@ -1,3 +1,18 @@
+library(shiny)
+library(dplyr)
+library(ggplot2)
+library(tidyverse)
+library(usmap)
+library(plotly)
+library(scales)
+
+
+co2_dataset <- read.csv("https://raw.githubusercontent.com/owid/co2-data/master/owid-co2-data.csv")
+
+#which country has the highest CO2 emission in which country has the least?
+#Which country has the highest population which country has the lowest population? 
+#Which year has the most CO2 emissions in which year has the least CO2 emissions?
+
 server <- function(input, output) {
   compare1 <- reactive ({
     compare1 <- df1 %>%
